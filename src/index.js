61842b3b5e40c2680d3cfc73984b7535ce858dec
natistd <145734298+natistd@users.noreply.github.com>
2023-11-20 18:26:49 -0600

    JS Functions for search engine

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function displayWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "4040o4d2cfd656953bb07308ffet858a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
 function useSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#seach-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchElement = document.querySelector("#search-form");
 searchElement.addEventListener("submit", useSearchSubmit);
+
+searchCity("Mexico-City");
