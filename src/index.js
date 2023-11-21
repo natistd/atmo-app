fa1f3c410d4569c33c832ec6ad942223017e3234
natistd <145734298+natistd@users.noreply.github.com>
2023-11-20 19:34:12 -0600

    Inject image to weather condition

--- src/index.js
+++ src/index.js
@@ -9,13 +9,17 @@ function displayWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   conditionDescriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-info-icon" />`;
   temperatureElement.innerHTML = Math.round(temperature);
+
+  console.log(response.data);
 }
 
 function formatDate(date) {
